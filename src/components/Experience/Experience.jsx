8a800b7081583556e24d0e0c305cf9658516e2e1
OrensObenas <gilbertobenas@gmail.com>
2025-03-18 17:56:11 +0000

    Fixation de l'affichage des images lors du build

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -2,14 +2,64 @@ import React from 'react'
 import skills from "../../data/skills.json"
 import history from "../../data/history.json"
 import styles from "./Experience.module.css"
+import html from "../../../assets/skills/html.png"
+import js from "../../../assets/skills/First_js.png"
+import css from "../../../assets/skills/css.png"
+import react from "../../../assets/skills/react.png"
+import django from "../../../assets/skills/First_django.png"
+import mysql from "../../../assets/skills/First_mysql.png"
+import docker from "../../../assets/skills/First_docker.png"
+import git from "../../../assets/skills/First_git.png"
+import github from "../../../assets/skills/First_github.png"
+
 const Experience = () => {
+
+    let my_Skill = [
+        {
+          "title": "HTML",
+          "imageSrc": html
+        },
+        {
+          "title": "JS",
+          "imageSrc":js
+        },
+        {
+          "title": "CSS",
+          "imageSrc": css
+        },
+        {
+          "title": "React",
+          "imageSrc": react
+        },
+        {
+          "title": "Django",
+          "imageSrc": django
+        },
+        {
+          "title": "Mysql",
+          "imageSrc": mysql
+        },
+        {
+          "title": "Docker",
+          "imageSrc": docker
+        },
+        {
+          "title": "Git",
+          "imageSrc": git
+        },
+        {
+          "title": "Github",
+          "imageSrc": github
+        }
+    ];
+
   return (
     <section id="experience" className={styles.container}>
         <h2 className={styles.title}>Skills</h2>
         <div className={styles.content} >
             <div className={styles.skills}>
                 {
-                    skills.map((skills, id) => {
+                    my_Skill.map((skills, id) => {
                         return <div key={id} className={styles.skill}>
                             <div className={styles.skillImageContainer}>
                                 <img src={skills.imageSrc} alt={skills.title}/>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,14 +2,54 @@ import React from 'react'
 import projects from "../../data/projects.json"
 import ProjectCard from './ProjectCard';
 import styles from './Projects.module.css'
+import obeisity from '../../../assets/projects/Obeisity.png'
+import chatapp_new from '../../../assets/projects/Chatapp_new.png'
+import gallery from "../../../assets/projects/Galery.png"
+import rapido from "../../../assets/projects/Rapido.png"
 
 const Projects = () => {
+  
+  let ForSee = [
+    {
+      "title": "Prediction of Obesity",
+      "imageSrc": obeisity,
+      "description": "Application based on a machine learning model capable of predicting whether a person is obese or not.",
+      "skills": ["React", "Express", "Node"],
+      "demo": "https://www.example.com",
+      "source": "https://github.com/OrensObenas/Coding_week"
+    },
+    {
+      "title": "Chatapp",
+      "imageSrc": chatapp_new,
+      "description": "Instant web chat application that ensures optimal security for its users.",
+      "skills": ["React", "Express", "Node", "Sass"],
+      "demo": "https://www.example.com",
+      "source": "https://github.com/OrensObenas/chatApp"
+    },
+    {
+      "title": "Gallery Management",
+      "imageSrc":gallery,
+      "description": "Web gallery management application that allows images to be categorized with multiple attributes.",
+      "skills": ["React", "Express", "Node"],
+      "demo": "https://www.example.com",
+      "source": "https://www.github.com"
+    },
+    {
+      "title": "Taxi Course Management",
+      "imageSrc": rapido,
+      "description": "Web-based course management application that allows a company to manage all the activities.",
+      "skills": ["React", "Express", "Node"],
+      "demo": "https://www.example.com",
+      "source": "https://www.github.com"
+    }
+];
+
   return (
     <section className={styles.container} id='projets'>
         <h2 className={styles.title}>Projects</h2>
         <div className={styles.projects}>
             {
-                projects.map((project, id) => {
+                ForSee.map((project, id) => {
                     return <ProjectCard key={id} project={project} />
                 })
             }
